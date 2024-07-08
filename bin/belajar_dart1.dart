ac9ed8d2ec4dd6f9d28715ea38b387c618557c76
Gamahusen9 <gamahusen1@gmail.com>
2024-07-08 10:46:03 +0700

    first commit

---
+++ bin/belajar_dart1.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart1/belajar_dart1.dart' as belajar_dart1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart1.calculate()}!');
+}
---
+++ bin/variabel.dart
@@ -0,0 +1,51 @@
+void main() {
+  // ini adalah tipe data var bisa berisi tipe data apapun
+  var vRandom = 'gama husen';
+  var vNumber = 12345;
+  var vBool = true;
+  var vDouble = 12.5;
+
+  print('VAR: $vNumber, $vRandom, $vBool, $vDouble ');
+
+  // ini adalah tipe data final(tidak bisa dideklarasi ulang, tetapi bisa dirubah isinya lebih dipakai saat menggunakan array)
+  final fAngka = 1;
+  print('FINAL: $fAngka');
+
+  // ini adalah tipe data late variabel dideklarasi setelah dipanggil
+  late var value = getValue();
+  print('LATE: display value');
+  print('LATE: $value');
+
+  // ini adalah tipe data cosntant(tidak bisa didelakrasi ulang, tidak bisa juga dirubah isinya)
+  const cAngka = 1;
+  print('CONST: $cAngka');
+
+  // ini adalah tipe data string
+  String strName = "Gama Husen";
+  print('STRING: $strName');
+
+// ini adalah tipe data integer
+  int intAngka = 12;
+  print('NUMBER: $intAngka');
+
+  // ini adalah tipe data bool
+  double doubleAngka = 12.5;
+  print('DOUBLE: $doubleAngka');
+
+  // ini adalah tipe data num(bisa menggunakan integer maupun boolean)
+  num numInteger = 12;
+  print('NUM: $numInteger');
+  num numDouble = 12.5;
+  print('NUM: $numDouble');
+
+  // ini adalah tipe data boolean
+  bool boolKondisiTrue = true;
+  print(boolKondisiTrue);
+  bool boolKondisiFalse = false;
+  print(boolKondisiFalse);
+}
+
+String getValue() {
+  print('LATE: getvalue()');
+  return 'LATE: gama husen';
+}
